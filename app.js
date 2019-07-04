8997133f262b489dea12b5767def5042af1c6268
Azdren <azdrenymeri@outlook.com>
2019-07-04 10:18:08 +0200

    dom implementation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const GameBoard = ( ()=>{
-    
+
     let CurrentBoard = document.querySelectorAll('.Box');
 
     let Board = [["","",""],
@@ -17,15 +17,15 @@ const playerFactory = (name, mark) => {
   };
 
   Check_The_Winning_Status = (mark)=>{
-    
+
     for(var i = 0 ; i < 3 ; i++){
         if(Board[i][0] == mark && Board[i][1] == mark && Board[i][2] == mark){return true;}
     }
-  
-   for(var i = 0 ; i < 3 ; i++){  
-        if(Board[0][i] == mark && Board[1][i] == mark && Board[2][i] == mark){  return true;} 
+
+   for(var i = 0 ; i < 3 ; i++){
+        if(Board[0][i] == mark && Board[1][i] == mark && Board[2][i] == mark){  return true;}
     }
-  
+
    if(Board[0][0] == mark && Board[1][1] == mark && Board[2][2] == mark){ return true;}
    if(Board[0][2] == mark && Board[1][1] == mark && Board[2][0] == mark){return true;}
     return false;
